bfc1cd4f31cb26226f61ef7f9b56196e2154c9dd
Alien Zeus <moyurponkhy@gmail.com>
2025-02-10 02:30:04 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Dark Mode Toggle
+const themeToggleBtn = document.getElementById('theme-toggle');
+const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+if (
+  localStorage.getItem('color-theme') === 'dark' ||
+  (!('color-theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark');
+  if (document.documentElement.classList.contains('dark')) {
+    localStorage.setItem('color-theme
